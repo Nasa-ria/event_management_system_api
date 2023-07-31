c9de22b4b4f94397a268e7d06c5c23be83160d28
Nasaria Mumuni <nasaria@walulel.com>
2023-07-31 16:55:04 +0000

    added a barcode,ticket scan ,pyment method and a barcode generator

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Models\User; 
 use App\Models\Ticket;
 use App\Mail\TicketMail;
-use App\Models\User; 
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Picqer\Barcode\BarcodeGeneratorHTML;
 
 class TicketController extends Controller
 { 
@@ -36,43 +37,94 @@ class TicketController extends Controller
 
     }
 
-    public  function purchaseTicket(Request $request){
-        $request->validate([
-            'quantity' => 'required',
-            'email' => 'required',
-        ]);
-        
-        $ticketCode = [];
-        for ($x = 0; $x < $request->quantity; $x++) {
-            $ticket = $this->createTicket($request);
-            // Create uuid
-            $ticketCode[] = Str::uuid();
-    $uniqueCode = array_push($ticketCode); // Generate a unique code as a string
+    public function purchaseTicket(Request $request)
+{
+    $request->validate([
+        'quantity' => 'required',
+        'email' => 'required',
+        'ticket_type' => 'required' // Corrected the field name 'ticket type' to 'ticket_type'
+    ]);
 
-            $ticket->status = "purchase"; // Set the status attribute
-            $ticket->email = $request->email; // Set the email attribute
-            $ticket->uniqueCode = json_encode( $ticketCode); // Set the uniqueCode attribute
-            $ticket->save(); // Save the changes to the ticket
+    // Create a new ticket and assign the attributes based on the request data
+    $ticket = $this->createTicket($request);
+    $ticket->purchase = $ticket->status; // Set the status attribute (assuming status exists as an attribute in the Ticket model)
+    // If you want to set the status based on some specific condition, update this line accordingly.
+    $ticket->email = $request->email; // Set the email attribute
+    $ticket->ticket_code = json_encode($this->generateBarcode($request->quantity)); // Set the ticket_code attribute using the generated barcode
+    $ticket->ticket_type = $request->input('ticket_type'); // Set the ticket_type attribute
+
+    // Save the changes to the ticket
+    $ticket->save();
+
+    // Update the attendees count for the event
+    $event = Event::where('event', '=', request()->event_name)->first();
+    $event->decrement('attendees');
+    if ($event->attendees == 0) {
+        return "sold out";
+    }
+
+    // Send the ticket information to the user via email
+    Mail::to($request->email)->send(new TicketMail($ticket, $event));
+
+    return $ticket;
+}
+    private function generateBarcode($quantity)
+    {
+        // Assuming you want to generate multiple barcodes based on the given quantity.
+        $generator = new BarcodeGeneratorHTML();
+        $barcodes = [];
         
-            $ticketCode[] = $uniqueCode; // Store the uniqueCode in the array for later use
+        for ($i = 0; $i < $quantity; $i++) {
         
-            // $user= $request->user()->id;
-            $event = Event::where('event', '=', request()->event_name)->first();
-            $event->decrement('attendees');
-            if ($event->attendees == 0) {
-                return "sold out";
-            }
+            $ticketCode = 'TCK-' . strtoupper(Str::random(8));
+            $barcodes[] = $ticketCode;
         }
         
-        $email = $request->email;
-        Mail::to($email)->send(new TicketMail($ticket,$event));
-        return $ticket;
+        return $barcodes;
+    }
+    
+
+      public function scanTicket($ticketCode)
+    {
+        $ticket = Ticket::where('ticket_code', $ticketCode)->first();
+        
+        if ($ticket) {
+            // Ticket found, do something with the ticket information
+            return response()->json(['ticket' => $ticket]);
+        } else {
+            // Ticket not found in the database
+            return response()->json(['error' => 'Ticket not found'], 404);
+        }
     }
 
-    public function events(){
-        $events=  Event::all();
-         return response()->json([
-            "users"=>$events,
-         ]);
+    public function processPayment(Request $request)
+    {
+        // Get payment gateway credentials from configuration
+        $gatewayKey = config('app.payment_gateway_key');
+        $gatewaySecret = config('app.payment_gateway_secret');
+
+        // Initialize the payment gateway SDK
+        $paymentGateway = new AwesomeSDK($gatewayKey, $gatewaySecret);
+
+        // Gather payment data from the request (e.g., amount, currency, etc.)
+        $amount = $request->input('amount');
+        $currency = $request->input('currency');
+        // ...
+
+        // Create a payment transaction with the gateway
+        $transactionId = $paymentGateway->createTransaction($amount, $currency);
+
+        // Handle successful payment response
+        if ($transactionId) {
+            // Process the successful payment and update your database accordingly
+            // For example, save the transaction ID and update the order status
+
+            return response()->json(['message' => 'Payment successful!', 'transaction_id' => $transactionId]);
+        } else {
+            // Handle failed payment
+            // For example, redirect back with an error message
+            return back()->with('error', 'Payment failed. Please try again.');
+        }
     }
+   
 }
