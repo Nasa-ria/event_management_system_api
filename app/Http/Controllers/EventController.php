ab351db219c583487193254b463c22ebf7cbe45e
Nasaria Mumuni <nasaria@walulel.com>
2023-07-31 16:58:43 +0000

    added a submitFeedback function

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -1,16 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Google_Client;
 use Google\Service;
 use App\Models\Event;
 use App\Mail\EventMail;
-// use Google_Service_Calendar;
+use App\Models\Feedback;
 use Google\Service\Calendar;
+use Google_Service_Calendar;
 use Illuminate\Http\Request;
 use App\Models\Eventpromotion;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+// use App Google\Service\Calendar\Calendar;
 
 
 class EventController extends Controller
@@ -79,11 +81,25 @@ class EventController extends Controller
         // require_once require_once './vendor/autoload.php';
         // Initialize Google API client with your credentials
         $client = new Google_Client();
+        $service_account_name = getenv('GOOGLE_SERVICE_ACCOUNT_NAME');
+        $key_file_location = base_path() . getenv('GOOGLE_KEY_FILE_LOCATION');
+
+        $key = file_get_contents($key_file_location);
         $client->setAuthConfig('C:\Users\WALULEL\Downloads\event-management-system-api-34a0fb4aff73.json');
         $client->addScope(Google_Service_Calendar::CALENDAR_EVENTS);
+        $scopes = ['https://www.googleapis.com/auth/calendar'];
 
+        $cred = new \Google_Auth_AssertionCredentials(
+            $service_account_name,
+            $scopes,
+            $key
+        );
         // Create a service to interact with the Google Calendar API
         $service = new Google_Service_Calendar($client);
+        $client->setAssertionCredentials($cred);
+        $this->app->singleton(\Google_Service_Calendar::class, function ($app) use ($client) {
+            return new \Google_Service_Calendar($client);
+        });
 
         // Create a new event in Google Calendar
         $calendarEvent = new Google_Service_Calendar_Event([
@@ -99,4 +115,46 @@ class EventController extends Controller
         $event->update(['google_calendar_event_id' => $createdEvent->id]);
         return response()->json(['message' => 'Event added to calender successfully']);
     }
+
+    public function calender(Google_Service_Calendar $client){
+        $calendarList = $client->calendarList->listCalendarList();
+        
+}
+public function submitFeedback(Request $request, $id)
+{
+    $event_id = Event::findOrFail($id);
+
+    $validatedData = $request->validate([
+        'feedback' => 'required|string|max:255',
+        'event_id'  =>'required'
+    ]);
+
+    // You can store the feedback in the database or perform any other actions here.
+    $feedback = Feedback ::create([
+        'feedback' => $request->feedback,
+        'event_id'=>$event_id
+    ]);
+    return $feedback;
+}
+
+public function events(){
+    $events=  Event::all();
+     return response()->json([
+      
+        "events"=>$events,
+     ]);
+}
+
+
+public function event(Request $request, Event $event){
+
+    $feedbacks = Feedback::where('event_id', '=', $event->id)->get();
+    if($feedbacks){
+            return response()->json([
+                "feedbacks"=>$feedbacks,
+             ]);
+  
+    }
+   
+}
 }
